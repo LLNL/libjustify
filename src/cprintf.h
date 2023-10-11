442aaf34df1d8b7db62b3ee6f07db8a711ea3af8
Stephanie Brink <brink2@llnl.gov>
2023-10-11 10:39:11 -0700

    update header
    move header guard to beginning of file

--- src/cprintf.h
+++ src/cprintf.h
@@ -3,22 +3,28 @@
 //
 // SPDX-License-Identifier: MIT
 
-#include <stdio.h>
-#include <stdarg.h>
 #ifndef __CPRINTF_H_HEADER
 #define __CPRINTF_H_HEADER
+
+#include <stdio.h>
+#include <stdarg.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void cprintf(const char *fmt, ...);
+
 void cfprintf(FILE *stream, const char *fmt, ...);
 
 void cvprintf(const char *fmt, va_list args);
+
 void cvfprintf(FILE *stream, const char *fmt, va_list args);
+
 void dump_graph();
+
 void cflush(void);
+
 #endif
 
 #ifdef __cplusplus
